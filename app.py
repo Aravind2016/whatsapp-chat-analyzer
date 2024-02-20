81ff731aeb46f7f1d2bc511adeb41371e58dc8a7
Aravind2016 <aravindbobbala@gmail.com>
2024-02-20 14:03:59 +0530

    added

--- app.py
+++ app.py
@@ -92,11 +92,11 @@ if uploaded_file is not None:
                 st.pyplot(fig)
             with col2:
                 st.dataframe(new_df)
-        # st.title('wordcloud')
-        # df_wc=helper.create_wordcloud(selected_user,df)
-        # fig,ax=plt.subplots()
-        # ax.imshow(df_wc)
-        # st.pyplot(fig)
+        st.title('wordcloud')
+        df_wc=helper.create_wordcloud(selected_user,df)
+        fig,ax=plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
 
         most_common_df = helper.most_common_words(selected_user, df)
 
