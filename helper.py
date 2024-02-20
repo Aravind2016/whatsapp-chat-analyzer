83d6a1b6fee01a91327599f5a0f73ff50ae4e4d2
Aravind2016 <aravindbobbala@gmail.com>
2024-02-20 13:13:33 +0530

    added

--- helper.py
+++ helper.py
@@ -33,25 +33,25 @@ def most_busy_users(df, selected_user='overall'):
         columns={'index': 'name', 'user': 'percent'})
     return x, df_percent
 
-def create_wordcloud(selected_user, df):
-    if selected_user != 'overall':
-        df = df[df['user'] == selected_user]
-    f = open('stop_hinglish.txt', 'r')
-    stop_words = f.read()
-    temp = df[df['user'] != 'group_notification']
-    temp = temp[temp['message'] != '<Media omitted>\n']
-
-    def remove_stop_words(message):
-        y = []
-        for word in message.lower().split():
-            if word not in stop_words:
-                y.append(word)
-        return " ".join(y)
-
-    wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
-    temp['message'] = temp['message'].apply(remove_stop_words)
-    df_wc = wc.generate(temp['message'].str.cat(sep=" "))
-    return df_wc
+# def create_wordcloud(selected_user, df):
+#     if selected_user != 'overall':
+#         df = df[df['user'] == selected_user]
+#     f = open('stop_hinglish.txt', 'r')
+#     stop_words = f.read()
+#     temp = df[df['user'] != 'group_notification']
+#     temp = temp[temp['message'] != '<Media omitted>\n']
+#
+#     def remove_stop_words(message):
+#         y = []
+#         for word in message.lower().split():
+#             if word not in stop_words:
+#                 y.append(word)
+#         return " ".join(y)
+#
+#     wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
+#     temp['message'] = temp['message'].apply(remove_stop_words)
+#     df_wc = wc.generate(temp['message'].str.cat(sep=" "))
+#     return df_wc
 
 def most_common_words(selected_user, df):
     if selected_user != 'overall':
